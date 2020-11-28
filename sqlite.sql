97f32ea1b412927b4746b2c5ca5a875631605fed
bruden <bruden@users.noreply.github.com>
2020-11-28 20:05:34 +0800

    Modify the database table.

--- sqlite.sql
+++ sqlite.sql
@@ -9,7 +9,7 @@ Target Server Type    : SQLite
 Target Server Version : 30714
 File Encoding         : 65001
 
-Date: 2020-11-21 19:18:17
+Date: 2020-11-28 20:04:24
 */
 
 PRAGMA foreign_keys = OFF;
@@ -20,7 +20,9 @@ PRAGMA foreign_keys = OFF;
 CREATE TABLE "song" (
 "title"  TEXT NOT NULL,
 "creator"  TEXT NOT NULL,
+"createTime"  INTEGER NOT NULL,
+"downloadCount"  INTEGER NOT NULL,
 "duration"  INTEGER NOT NULL,
 "tones"  BLOB NOT NULL,
-PRIMARY KEY ("title")
+PRIMARY KEY ("title" ASC)
 );
